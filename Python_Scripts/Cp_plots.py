aebc326133a38a5d241705a72319ddc7f1c30cda
JanekWiacek <133200470+JanekWiacek@users.noreply.github.com>
2023-11-12 17:02:20 +0100

    plotting code residual

--- Python_Scripts/Cp_plots.py
+++ Python_Scripts/Cp_plots.py
@@ -11,7 +11,7 @@ linewidth_global = 0.6
 
 def import_data(path: str, exp=True) -> np.ndarray:
     if exp:
-        n_skip_head = 2
+        n_skip_head = 1
         n_skip_foot = 1
 
         return np.genfromtxt(path, skip_header=n_skip_head, skip_footer=n_skip_foot)
@@ -26,22 +26,25 @@ def import_data(path: str, exp=True) -> np.ndarray:
 
 def format_cfd_data(data: np.ndarray) -> np.ndarray:
     formatted_data = np.zeros(shape=data.shape)
-    formatted_data[:, 0] = ((data[:, 0] - np.min(data[:,0]))/ np.max(data[:, 0]))
+    formatted_data[:, 0] = (data[:, 0]- np.min(data[:,0]))/ np.max(data[:, 0])*0.9286
     formatted_data[:, 1] = data[:,1]
 
     return formatted_data
 
 
-def plot_pressure_coeff(exp_data: np.ndarray, cfd_data=[], cfd=False) -> None:
-    plt.plot(exp_data[:, 0], exp_data[:, 1], linewidth=linewidth_global, color=color_exp, label="Experimental")
+def plot_pressure_coeff(exp_data: np.ndarray, cfd_data1=[], cfd_data2=[], cfd=False) -> None:
+    plt.plot(exp_data[:, 0], exp_data[:, 1], linewidth=0.8, color="k", label="Experimental")
 
     if cfd:
-        plt.scatter(cfd_data[:, 0], cfd_data[:, 1], color=color_cfd, marker=".", s=0.6, label="CFD")
+        plt.scatter(cfd_data1[:, 0], cfd_data1[:, 1], color="r", marker="x", s=10, label="coarse grid")
+        plt.scatter(cfd_data2[:, 0], cfd_data2[:, 1], color="b", marker=".", s=10, label="fine grid")
 
-    plt.legend()
+
+    plt.legend(fontsize=18)
     plt.gca().invert_yaxis()
-    plt.xlabel("x/c")
-    plt.ylabel("$c_{p}$")
+    plt.xlabel("x/c", size=20)
+    plt.ylabel("$c_{p}$",size=20)
+    plt.grid()
     plt.show()
 
     return None
@@ -67,6 +70,7 @@ wing_cfd_6_upwind = format_cfd_data(import_data("Cp_wing_6_upwind.csv", False))
 wing_cfd_6_SSG = format_cfd_data(import_data("Cp_wing_6_SSG.csv", False))
 
 
-plot_pressure_coeff(wing_exp_13, wing_cfd_13_f, True)
+plot_pressure_coeff(wing_exp_6, wing_cfd_6_c, wing_cfd_6_f, True)
+
 
 print(import_data("Cp_flap_6_c.csv",False))
---
+++ Python_Scripts/residual_plots.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pylab
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+
+# Plotting Parameters
+color_cfd = "k"
+color_exp = "r"
+linewidth_global = 0.6
+
+
+def import_data(path: str, exp=True) -> np.ndarray:
+
+    n_skip_head = 6
+    n_skip_foot = 1
+
+    return np.genfromtxt(path, skip_header=n_skip_head, skip_footer=n_skip_foot)[:, (0, -1)]
+
+
+
+
+def format_cfd_data(data: np.ndarray) -> np.ndarray:
+    formatted_data = np.zeros(shape=data.shape)
+    formatted_data[:, 0] = data[:, 0]
+    formatted_data[:, 1] = data[:,1]
+
+    return formatted_data
+
+
+def plot_residual(res1=[], res2=[], res3=[]) -> None:
+    plt.plot(res1[:, 0], res1[:, 1], linewidth=1.5, color="k", label="AoA 6 coarse mesh")
+    plt.plot(res2[:, 0], res2[:, 1], linewidth=1.5, color="b", label="AoA 6 fine mesh")
+    plt.plot(res3[:, 0], res3[:, 1], linewidth=1.5, color="r", label="AoA 13.1 fine mesh")
+
+    plt.yscale('log')
+    plt.legend(fontsize=18)
+    plt.xlabel("Iterations", size=20)
+    plt.ylabel("RMS U-Mom",size=20)
+    plt.grid()
+    plt.show()
+
+    return None
+
+#read residual data
+
+res_6_c = format_cfd_data(import_data("residuals_6_c.csv"))
+res_6_f = format_cfd_data(import_data("residuals_6_f.csv"))
+res_13_f = format_cfd_data(import_data("residual_131_f.csv"))
+res_6_SSG = format_cfd_data(import_data("residual_6_SSG.csv"))
+res_6_upwind = format_cfd_data(import_data("residuals_6_upwind.csv"))
+
+
+plot_residual(res_6_c, res_6_f, res_13_f)
+
+
+#print(import_data("Cp_flap_6_c.csv",False))
